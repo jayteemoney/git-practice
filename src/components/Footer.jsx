7d7134938e6b2995acddc49186deae64d21706df
jayteemoney <jethroirmiya@gmail.com>
2025-05-21 09:10:31 +0000

    added add-footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple project to practice Git and GitHub.</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
